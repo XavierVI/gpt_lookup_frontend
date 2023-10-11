9609defcee88a7690543a4722c19e11fbb972597
xavier <vigenesisvi@gmail.com>
2023-10-11 17:50:21 -0600

    added logic for making requests to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  userIn: string = '';
   
-  sendUserInput(userIn: string): void {
-    const url: string = 'localhost:8000';
+  res: any;
+
+  constructor(private httpClient: HttpClient) { }
+  ngOnInit(): void {  }
+
+  sendUserInput(): void {
+    const url: string = `localhost:8000/gpt`;
+    const json = { userIn: this.userIn };
     // Makes request to backend for results
-    
+    this.httpClient.post<any>(url,json).subscribe(res => {
+      this.res = res;
+    })
   }
 }
